0a17c594370ee9f0e85247d3142de8ed2413024d
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2016-12-08 17:33:37 +0100

    Update template branch

--- tests/test_pymarkdoc.py
+++ tests/test_pymarkdoc.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
+
+"""Main test script."""
+
+
 from click.testing import CliRunner
 
 from pymarkdoc.cli import main
 
 
 def test_main():
+    """Main test method."""
+
     runner = CliRunner()
     result = runner.invoke(main, [])
 
